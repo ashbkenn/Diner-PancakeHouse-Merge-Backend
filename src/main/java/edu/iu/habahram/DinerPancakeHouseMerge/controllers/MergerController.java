f5d8b60f950858683fe7ff8e112b1ff2c0a64f83
Ashlyn Kennedy <ashbkenn@iu.edu>
2024-04-01 11:01:59 -0400

    part 7

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,15 +1,13 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
-import edu.iu.habahram.DinerPancakeHouseMerge.model.Menu;
-import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItemRecord;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 @RestController
@@ -24,17 +22,8 @@ public class MergerController {
     }
 
     @GetMapping
-    public List<MenuItem> get() {
-        List<MenuItem> menuItems = new ArrayList<>();
-
-        ArrayList<Menu> menus = mergerRepository.getTheMenus();
-        for (Menu menu : menus) {
-            Iterator<MenuItem> menuIterator = menu.createIterator();
-            while (menuIterator.hasNext()) {
-                menuItems.add(menuIterator.next());
-            }
-        }
-
-        return menuItems;
+    public List<MenuItemRecord> get() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        return items;
     }
 }
--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/Menu.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/Menu.java
@@ -1,7 +1,46 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.model;
 
-import java.util.Iterator;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuComponent;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
 
-public interface Menu {
-    Iterator<MenuItem> createIterator();
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu extends MenuComponent{
+    ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return (MenuComponent)menuComponents.get(i);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public MenuItem[] getItems() {
+        ArrayList<MenuItem> items = new ArrayList<>();
+        for (MenuComponent menuComponent : menuComponents) {
+            items.addAll(List.of(menuComponent.getItems()));
+        }
+        return items.toArray(new MenuItem[0]);
+    }
 }
